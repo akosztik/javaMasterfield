8dd015e0fc29ebb52ce292fe450d9d64417c7de8
Kosztik <anett.kosztik@evosoft.com>
2018-02-15 09:43:42 +0100

    chatserver

--- ChatServer/src/Server/ChatServer.java
+++ ChatServer/src/Server/ChatServer.java
@@ -2,14 +2,22 @@ package Server;
 
 import java.io.*;
 import java.net.*;
+import java.util.Vector;
 
-public class ChatServer {
+public class ChatServer{
 
 	public static void main(String[] args) {
+		Vector<ServerThread> threads= new Vector<>();   //diamond szintaktika
 		try {
 			ServerSocket ss = new ServerSocket(1035);
-			ss.accept();
-			System.out.println("több kapcsolat érkezett");
+			while (true) {
+				Socket sock=ss.accept();
+				ServerThread st =new ServerThread(sock,threads);// címszerinti átadás, ezért látja mindenki ha belép egy új ember a chatszobába.
+				threads.add(st);
+				st.netWrite("Hello!");
+				
+				System.out.println("kapcsolat érkezett");
+			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
